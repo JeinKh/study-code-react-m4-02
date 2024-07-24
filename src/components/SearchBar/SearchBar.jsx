0f01ff71304667420b78763059f3f7ff9799832b
JeinKh <154812936+JeinKh@users.noreply.github.com>
2024-07-24 14:39:25 +0300

    Initial commit

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,21 @@
+import { Form, Formik, Field } from "formik";
+const SearchBar = ({ setQuery }) => {
+  const initialValues = {
+    query: "",
+  };
+  const handleSubmit = (values) => {
+    setQuery(values.query);
+  };
+  return (
+    <div>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <Field name="query" placeholder="Enter search value" type="search" />
+          <button type="submit">Search</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default SearchBar;
